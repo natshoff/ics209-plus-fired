8c151de55a3f7de49289b575a41b5e1137a2c06a
Maxwell Cook <maxwell.cook@colorado.edu>
2022-02-11 12:17:32 -0700

    Added GRIDMET summary for western states ([vpd,fm1000])
    Added GRIDMET summary for western states ([vpd,fm1000])

---
+++ code/R/gridmet.R
@@ -0,0 +1,18 @@
+# LIBRARIES
+
+library(tidyverse)
+
+# Read in the GRIDMET summaries, tidy
+
+vpd <- read.csv("data/tabular/mod/gridmet/gridmet_states_month_avg_vpd.csv")
+
+fm1000 <- read.csv("data/tabular/mod/gridmet/gridmet_states_month_avg_fm1000.csv")
+# Tidy, overwrite
+
+vpd %>% 
+ select(NAME, STUSPS, GEOID, contains('_vpd')) %>%
+ write.csv(., "data/tabular/mod/gridmet/gridmet_states_month_avg_vpd.csv")
+
+fm1000 %>% 
+ select(NAME, STUSPS, GEOID, contains('_fm1000')) %>%
+ write.csv(., "data/tabular/mod/gridmet/gridmet_states_month_avg_vpd.csv")
